6d9a621802fb187c4886f79487f14fa14ddcd1d8
kushinoyuya <kusshy15059855@gmail.com>
2020-09-11 18:53:51 +0900

    20200911

--- main/excel.py
+++ main/excel.py
@@ -1,12 +1,14 @@
 import os
 import glob
 import openpyxl
+import datetime
 
 from pathlib import Path
 
 def get_path():
-    '''対象のエクセルファイルのディレクトリを指定する'''
-    path = os.path.isfile
-
-    with open('temp/file.txt', 'w') as f:
-    f.write('')
+    '''対象のエクセルファイルを選択する'''
+    wb = openpyxl.load_workbook('./excel_project/sample.xlsx')
+    ws = wb.worksheets[-1]
+    wb.copy_worksheet(ws)
+    ws.title = 'YYYYMM'
+    wb.save('./excel_project/sample.xlsx')
